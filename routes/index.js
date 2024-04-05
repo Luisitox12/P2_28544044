84a5d3de035d89c4db3cbc39fbe30092be16a62f
Luisitox12 <luishidalgops6@gmail.com>
2024-04-05 14:12:26 -0400

    Primer Cambio

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hola Mundo siuuuuu', 
+    name: 'Luis',
+    secondname: 'Hidalgo',
+    id: '28544044',
+    mat: 'Programacion 2',
+    sec: 'Seccion 4',
+});
+});
+
+module.exports = router;
