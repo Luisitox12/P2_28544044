cdb8b6a27463207285292aaace04036f01c7ceb3
Luisitox12 <luishidalgops6@gmail.com>
2024-05-11 21:51:38 -0400

    data base

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const ContactosController = require("../controllers/ContactosController");
+const contactosController = new ContactosController();
+
+const indexController = require("../controllers/indexController")
+
+var express = require("express");
+var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Curriculum', 
-    name: 'Luis Augusto',
-    secondname: 'Hidalgo',
-    id: '28544044',
-    mat: 'Programacion 2',
-    sec: 'Seccion 4',
 });
 });
 
-router.get('/form', function(req, res, next){
-  res.send("Listo llamo a /form");
-});
+router.post("/form-contacto", contactosController.add);
+
 module.exports = router;
