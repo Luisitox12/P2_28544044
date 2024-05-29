5e89f0ae59cc01c940caae9a5e89a1c487087505
Luisitox12 <luishidalgops6@gmail.com>
2024-05-28 22:32:26 -0400

    correo

--- controllers/ContactosController.js
+++ controllers/ContactosController.js
@@ -1,10 +1,37 @@
 const ContactosModel = require("../models/ContactosModel");
+const nodemailer = require('nodemailer');
 
 class ContactosController {
   constructor() {
     this.contactosModel = new ContactosModel();
     this.add = this.add.bind(this);
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'pruebadeprogra@gmail.com',
+        pass: 'Venecia21.'
+      }
+    });
   }
+
+  //funcion enviar correo
+  enviarCorreo(email, nombre, mensaje) {
+    const mailOptions = {
+      from: 'pruebadeprogra@gmail.com',
+      to: 'luishidalgops6@gmail.com', // Agrega más destinatarios si es necesario
+      subject: 'Nuevo registro de usuario',
+      text: `Nombre: ${nombre}\nEmail: ${email}\nMensaje: ${mensaje}`
+    };
+  
+    this.transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`Correo enviado: ${info.response}`);
+      }
+    });
+  }
+// fin funcion enviar correo
 
   async add(req, res) {
     // Validar los datos del formulario
@@ -23,6 +50,7 @@ class ContactosController {
    
     try {
       await this.contactosModel.crearContacto(email, name, mensaje, ip, fecha);
+      this.enviarCorreo(email, name, mensaje);
       res.status(200).send("Tus datos se han enviado correctamente.");
     } catch (error) {
       res.status(500).send("Ha ocurrido un error al procesar tus datos.");
