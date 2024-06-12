0567c7cd8a0666f41a9f738cb3b57d78f788afae
Luisitox12 <luishidalgops6@gmail.com>
2024-06-12 08:22:20 -0400

    newvista

--- routes/contactos.js
+++ routes/contactos.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../conf/database');
 
 /* GET contactos page. */
 router.get('/', function(req, res, next) {
-  res.render('contactos', { title: 'Contactos' });
+  const query = 'SELECT email, name, mensaje, ip, fecha, pais FROM formulario'; // Reemplaza 'your_table_name' con el nombre de tu tabla
+
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.render('contactos', { title: 'Contactos', data: rows });
+  });
 });
 
 module.exports = router;
