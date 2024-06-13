edec588a6710cb9a663468198efc042582fda8c4
Luisitox12 <luishidalgops6@gmail.com>
2024-06-13 18:37:02 -0400

    res

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var bcrypt = require('bcrypt');
 var db = require('../conf/database'); // Ajusta según tu configuración de base de datos
 
-
 // Página de registro
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
@@ -14,12 +13,12 @@ router.post('/register', async function(req, res, next) {
   const { username, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    await db.run('INSERT INTO users (username, password) VALUES (?,?)', [username, hashedPassword]);
+    await db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword]);
     req.flash('success_msg', 'You are now registered and can log in');
     res.redirect('/auth/login');
   } catch (err) {
     console.error(err);
-    req.flash('error_msg', 'Datos Invalidos. Porfavor intente denuevo.');
+    req.flash('error_msg', 'Something went wrong. Please try again.');
     res.redirect('/auth/register');
   }
 });
@@ -30,12 +29,10 @@ router.get('/login', function(req, res, next) {
 });
 
 // Manejar inicio de sesión
-//MANEJAR INICIO DE SESION
-
 router.post('/login', async function(req, res, next) {
   const { username, password } = req.body;
   try {
-    const user = await db.get('SELECT * FROM users WHERE username =?', [username]);
+    const user = await db.get('SELECT * FROM users WHERE username = ?', [username]);
     if (!user) {
       req.flash('error_msg', 'No user found with that username');
       return res.redirect('/auth/login');
@@ -43,11 +40,6 @@ router.post('/login', async function(req, res, next) {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       req.flash('error_msg', 'Incorrect password');
-      return res.redirect('/auth/login');
-    }
-    if (!req.session) {
-      console.error('Error setting user in session');
-      req.flash('error_msg', 'Error setting user in session');
       return res.redirect('/auth/login');
     }
     req.session.user = user;
@@ -61,16 +53,12 @@ router.post('/login', async function(req, res, next) {
 
 // Manejar cierre de sesión
 router.get('/logout', function(req, res) {
-  if (req.session) {
-    req.session.destroy(err => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect('/auth/login');
-    });
-  } else {
+  req.session.destroy(err => {
+    if (err) {
+      return next(err);
+    }
     res.redirect('/auth/login');
-  }
+  });
 });
 
 module.exports = router;
