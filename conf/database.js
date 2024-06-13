43b01723f2c551daee428680a7c53fb4f6f0b00f
Luisitox12 <luishidalgops6@gmail.com>
2024-06-13 08:15:57 -0400

    db

--- conf/database.js
+++ conf/database.js
@@ -6,7 +6,7 @@ const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Could not connect to database', err);
   } else {
-    console.log('Connected to database');
+    console.log('Conectado a la base de Datos');
   }
 });
 
