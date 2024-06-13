23ede40051403b8a44505b4d6b532fcc58e09fe5
Luisitox12 <luishidalgops6@gmail.com>
2024-06-13 18:12:59 -0400

    lol

--- routes/auth.js
+++ routes/auth.js
@@ -2,34 +2,36 @@ var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt');
 var db = require('../conf/database'); // Ajusta según tu configuración de base de datos
+const flash = require('connect-flash');
+const session = require('express-session');
 
 // Página de registro
-router.get('/register', function(req, res, next) {
+router.get('/auth/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
 
 // Manejar registro
-router.post('/register', async function(req, res, next) {
+router.post('/auth/register', async function(req, res, next) {
   const { username, password } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     await db.run('INSERT INTO users (username, password) VALUES (?,?)', [username, hashedPassword]);
     req.flash('success_msg', 'You are now registered and can log in');
-    res.redirect('/views/login.ejs');
+    res.redirect('/auth/login');
   } catch (err) {
     console.error(err);
     req.flash('error_msg', 'Datos Invalidos. Porfavor intente denuevo.');
-    res.redirect('/views/register.ejs');
+    res.redirect('/auth/register');
   }
 });
 
 // Página de inicio de sesión
-router.get('/login', function(req, res, next) {
+router.get('/auth/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
 // Manejar inicio de sesión
-router.post('/login', async function(req, res, next) {
+router.post('/auth/login', async function(req, res, next) {
   const { username, password } = req.body;
   try {
     const user = await db.get('SELECT * FROM users WHERE username =?', [username]);
