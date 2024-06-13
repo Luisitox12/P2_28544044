b803f4db1a2d2d15243aa751d37a1f39fe96e439
Luisitox12 <luishidalgops6@gmail.com>
2024-06-13 14:22:44 -0400

    reqflash

--- routes/contactos.js
+++ routes/contactos.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../conf/database');
+var flash = require('connect-flash');
+
+router.use(flash());
 
 // Middleware de autenticación
 function ensureAuthenticated(req, res, next) {
