2e72b6e52d859c25a7332a1e391561e72d5a30dc
Luisitox12 <luishidalgops6@gmail.com>
2024-06-14 10:22:44 -0400

    xdxdxd

--- routes/contactos.js
+++ routes/contactos.js
@@ -17,26 +17,4 @@ function ensureAuthenticated(req, res, next) {
   }
 }
 
-
-// Middleware de autenticación
-function ensureAuthenticated(req, res, next) {
-  if (req.session && req.session.user) {
-    return next();
-  } else {
-    req.flash('error_msg', 'Por favor inicie sesión para ver los datos');
-    res.redirect('/auth/login');
-  }
-}
-
-// Ruta de contactos protegida
-router.get('/', ensureAuthenticated, async function(req, res, next) {
-  try {
-    const rows = await db.all('SELECT * FROM contactos');
-    res.render('contactos', { title: 'Contactos', data: rows });
-  } catch (err) {
-    console.error(err);
-    res.render('error', { message: 'Error retrieving contacts', error: err });
-  }
-});
-
 module.exports = router;
