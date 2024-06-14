923a3392638edaed2bbea12b74be8b3a9775ae1a
Luisitox12 <luishidalgops6@gmail.com>
2024-06-14 10:09:08 -0400

    db

--- routes/contactos.js
+++ routes/contactos.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../conf/datos');
+var db = require('../conf/datos.db');
 
 router.get('/', ensureAuthenticated, async function(req, res, next) {
   // El usuario ha iniciado sesión, mostrar la página de contactos
