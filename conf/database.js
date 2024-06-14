b4ecb65d0a6558e787c671392605fb69d60907da
Luisitox12 <luishidalgops6@gmail.com>
2024-06-14 10:14:28 -0400

    xdxd

---
+++ conf/database.js
@@ -0,0 +1,22 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, 'datos.db');
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Could not connect to database', err);
+  } else {
+    console.log('Conectado a la base de Datos');
+  }
+});
+
+// Crear tabla de usuarios si no existe
+db.serialize(function() {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT UNIQUE NOT NULL,
+      password TEXT NOT NULL
+    )
+  `);
+});
