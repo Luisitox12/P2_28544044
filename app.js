41830a24648fe1bd166c08d381f2b0cc78588168
Luisitox12 <luishidalgops6@gmail.com>
2024-06-19 06:21:29 -0400

    db

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
   clientID: process.env.USER_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "https://proyect-v0-1.onrender.com/auth/google/callback"
 },
 function(accessToken, refreshToken, profile, done) {
   return done(null, profile);
