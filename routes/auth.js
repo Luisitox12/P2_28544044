ea3fc91120a6ff2e62887ae5ffc5b7b1100a26b3
Luisitox12 <luishidalgops6@gmail.com>
2024-06-19 03:22:59 -0400

    stylebtn

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,7 @@ router.get('/google/callback',
     failureFlash: true 
   }),
   (req, res) => {
-    res.redirect('https://proyect-v0-1.onrender.com/contactos');
+    res.redirect('http://localhost:3000/contactos');
   }
 );
 
